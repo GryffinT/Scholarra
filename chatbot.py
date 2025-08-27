23e8a76d801df00e74c829451fe538314cccbddb
GryffinT <Gryffintubon@gmail.com>
2025-08-26 22:45:43 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1329,7 +1329,7 @@ if st.session_state.page == 7:
                     st.title("Lesson one, importing data")
                     st.write("In this lesson, you’ll learn how to bring data from outside sources into Excel. We’ll explore how to import information from both text files and CSV files, and see how Excel organizes that data so it’s ready for you to work with.")
 
-                    video_func("https://www.youtube.com/shorts/_5nGeEwx9ZI","How to Import PDF to Excel ⧸⧸ #shorts.mp4", "Mike Tholfsen", "How to import PDF into Excel" )
+                    url_video_func("https://www.youtube.com/shorts/_5nGeEwx9ZI", "Mike Tholfsen", "How to import PDF into Excel" )
 
                     segment_completed(1)
 
@@ -1341,15 +1341,15 @@ if st.session_state.page == 7:
 
                     # Video 1
                     
-                    video_func("https://www.youtube.com/watch?v=ovDpZD4BxQk","Microsoft Excel - Search for data within a workbook CC.mp4", "Kay Rand Morgan", "Search for data within a workbook" )
+                    url_video_func("https://www.youtube.com/watch?v=ovDpZD4BxQk", "Kay Rand Morgan", "Search for data within a workbook" )
 
                     # Video 2
                     
-                    video_func("https://www.youtube.com/watch?v=Z7RQnu3yrPk","Microsoft Excel - Navigate to named cells, ranges, or workbook elements CC.mp4", "Kay Rand Morgan", "Navigating to named cells, ranges, or workbook elements" )
+                    url_video_func("https://www.youtube.com/watch?v=Z7RQnu3yrPk", "Kay Rand Morgan", "Navigating to named cells, ranges, or workbook elements" )
 
                     # Video 3
 
-                    video_func("https://www.youtube.com/shorts/NIg7m4nv5Fg","How to Create & Remove Hyperlink？.mp4", "Santhu Analytics", "How to Create & Remove Hyperlinks" )
+                    url_video_func("https://www.youtube.com/shorts/NIg7m4nv5Fg", "Santhu Analytics", "How to Create & Remove Hyperlinks" )
                  
                     st.header("Test your knowledge with a short quiz to complete this section")
                     lesson_2_quiz_answers = ["Find & Select", "Yes", "An entry's column and row.", "Ctrl + K"]
@@ -1368,28 +1368,28 @@ if st.session_state.page == 7:
                 with lesson_three_expander:
                     st.title("Lesson 3, formatting")
                     st.write("In this lesson, you’ll learn how to format worksheets and workbooks, modify page setup for printing and presentation, adjust row height and column width, and customize headers and footers. These skills will help you organize data more effectively, improve the readability of your spreadsheets, and ensure your work is presented in a clear and professional manner.")
-                    video_func("https://www.youtube.com/watch?v=0SRt9dkR3Zg", "Excel Page Layout_ The Ultimate Guide.mp4","learnexcel.video","Excel Page Layout: The Ultimate Guide")
-                    video_func("https://www.youtube.com/shorts/l5UFTMXOuiU", "ssvid.net--How-to-AutoFit-rows-and-columns-in-Excel-shorts-excel_1080pFHR.mp4", "TutorialsPoint_", "How to AutoFit rows and columns in Excel?")
-                    video_func("https://www.youtube.com/watch?v=UbYcYXfHwII", "ssvid.net--Create-Custom-Headers-and-Footers-in-Excel_1080p.mp4", "Technology for Teachers and Students", "Create Custom Headers and Footers in Excel")
+                    url_video_func("https://www.youtube.com/watch?v=0SRt9dkR3Zg", "learnexcel.video","Excel Page Layout: The Ultimate Guide")
+                    url_video_func("https://www.youtube.com/shorts/l5UFTMXOuiU", "TutorialsPoint_", "How to AutoFit rows and columns in Excel?")
+                    url_video_func("https://www.youtube.com/watch?v=UbYcYXfHwII", "Technology for Teachers and Students", "Create Custom Headers and Footers in Excel")
                     segment_completed(3)
                 lesson_four_expander= st.expander(label="Lesson 3.1")
                 with lesson_four_expander:
                     st.title("Lesson 3.1, customization")
                     st.write("In this lesson, you’ll learn how to customize the Quick Access Toolbar, display and modify workbook content in different views, freeze worksheet rows and columns, change window views, modify basic workbook properties, and display formulas. Mastering these features will make navigating Excel more efficient, allow you to organize and review data with greater ease, and give you more control over how your workbook is displayed and managed.")
-                    video_func("https://www.youtube.com/watch?v=ERCg7RznD3w", "ssvid.net--Customize-the-Quick-Access-Toolbar-in-Excel_v720P.mp4", "Simon Sez IT", "Customize the Quick Access toolbar")
-                    video_func("https://www.youtube.com/watch?v=rqjStG5xTZ4", "ssvid.net--Microsoft-Excel-Display-and-Modify-workbook-content-in-different_1080p.mp4", "Kay Rand Morgan", "Display and modify workbook content in different views")
-                    video_func("https://www.youtube.com/shorts/SY9R06sqg3A", "ssvid.net--Freeze-or-lock-rows-and-columns-in-Excel_v720P.mp4", "SpreadsheetNation", "Freeze worksheet rows and columns")
-                    video_func("https://www.youtube.com/watch?v=GfHWyniYja4", "ssvid.net--Microsoft-Excel-Change-window-views-CC_1080p.mp4", "Kay Rand Morgan", "Change window views")
-                    video_func("https://www.youtube.com/watch?v=5ta5Vf8VRms", "ssvid.net--MO-200-Excel-Manage-Worksheets-and-Workbooks-Modify_v720P.mp4", "David Hays", "Modify basic workbook properties")
-                    video_func("https://www.youtube.com/watch?v=nBkv7EGsAIU", "ssvid.net--How-to-Show-Formulas-in-Excel_1080pFHR.mp4", "Excel Tutorials by EasyClick Academy", "Display formulas")
+                    url_video_func("https://www.youtube.com/watch?v=ERCg7RznD3w", "Simon Sez IT", "Customize the Quick Access toolbar")
+                    url_video_func("https://www.youtube.com/watch?v=rqjStG5xTZ4", "Kay Rand Morgan", "Display and modify workbook content in different views")
+                    url_video_func("https://www.youtube.com/shorts/SY9R06sqg3A", "SpreadsheetNation", "Freeze worksheet rows and columns")
+                    url_video_func("https://www.youtube.com/watch?v=GfHWyniYja4", "Kay Rand Morgan", "Change window views")
+                    url_video_func("https://www.youtube.com/watch?v=5ta5Vf8VRms", "David Hays", "Modify basic workbook properties")
+                    url_video_func("https://www.youtube.com/watch?v=nBkv7EGsAIU", "Excel Tutorials by EasyClick Academy", "Display formulas")
                     segment_completed(3.1)
                 lesson_five_expander= st.expander(label="Lesson 4")
                 with lesson_five_expander:
                     st.title("Lesson 4, how to configure for collaboration")
-                    video_func("https://www.youtube.com/watch?v=Mrt4v0ysA8w", "Lesson4Video1.mp4", "Excel Tutorials by EasyClick Academy", "How to Set the Print Area in Excel (Step by Step)")
-                    video_func("https://www.youtube.com/watch?v=P2L4GOGDsx8", "Lesson4Video2.mp4", "Kay Rand Morgan", "Microsoft Excel - Save workbooks in alternative file formats CC")
-                    video_func("https://www.youtube.com/watch?v=HfwMo6M1XzM", "Lesson4Video3.mp4", "Kevin Stratvert", "How to Print Excel Sheet")
-                    video_func("https://www.youtube.com/watch?v=KbJUKAY8FZ8", "Lesson4Video4.mp4", "How To Tutorials- Maha Gurus", "Inspecting and Protecting Workbooks- Inspect Document in Excel Tutorial")
+                    url_video_func("https://www.youtube.com/watch?v=Mrt4v0ysA8w", "Excel Tutorials by EasyClick Academy", "How to Set the Print Area in Excel (Step by Step)")
+                    url_video_func("https://www.youtube.com/watch?v=P2L4GOGDsx8", "Kay Rand Morgan", "Microsoft Excel - Save workbooks in alternative file formats CC")
+                    url_video_func("https://www.youtube.com/watch?v=HfwMo6M1XzM", "Kevin Stratvert", "How to Print Excel Sheet")
+                    url_video_func("https://www.youtube.com/watch?v=KbJUKAY8FZ8", "How To Tutorials- Maha Gurus", "Inspecting and Protecting Workbooks- Inspect Document in Excel Tutorial")
                     segment_completed(4)
                 lesson_six_expander = st.expander(label="Lesson 5")
                 with lesson_six_expander:
@@ -1407,6 +1407,7 @@ if st.session_state.page == 7:
                 st.warning("This course key is not accepted.")
         elif entered_course_key:
             st.error("Invalid course key.")
+
 
 
 
