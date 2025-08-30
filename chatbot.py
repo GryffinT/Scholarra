5140ab2ff2dd3d916676039fb811ca128f8d2e0a
Gryffin T <Gryffintubon@gmail.com>
2025-08-29 20:17:05 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1317,7 +1317,7 @@ if st.session_state.page >= 3:
     df = conn.read(worksheet="Sheet1", ttl=5)
 
     # Get the password from row 2, column 2
-    admin_password = df.iloc[1, 1]  # iloc[row_index, column_index]
+    admin_password = df.iloc[0, 1]  # iloc[row_index, column_index]
     st.title(admin_password)
 
     # Check if the entered key matches Admin password
@@ -1669,6 +1669,7 @@ if st.session_state.page == 7:
                 st.warning("This course key is not accepted.")
         elif entered_course_key:
             st.error("Invalid course key.")
+
 
 
 
